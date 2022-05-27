115082f9aa167b765c9ea73ff50c190a4c3b615c
Luan de Castro Ribeiro <luan.ribeiro@estudante.ifgoiano.edu.br>
2022-05-27 10:37:28 -0300

    Calculator creation

---
+++ Calculator/src/main/java/com/example/calculator/HelloController.java
@@ -0,0 +1,62 @@
+package com.example.calculator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+    @FXML
+    private TextField operationField;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private Button divisionButton;
+
+    @FXML
+    private Button multiplicationButton;
+
+    @FXML
+    private Button subtractionButton;
+
+    @FXML
+    private Button additionButton;
+
+    @FXML
+    private Button resultButton;
+
+    @FXML
+    private Button zeroButton;
+
+    @FXML
+    private Button oneButton;
+
+    @FXML
+    private Button twoButton;
+
+    @FXML
+    private Button threeButton;
+
+    @FXML
+    private Button fourButton;
+
+    @FXML
+    private Button fiveButton;
+
+    @FXML
+    private Button sixButton;
+
+    @FXML
+    private Button sevenButton;
+
+    @FXML
+    private Button eightButton;
+
+    @FXML
+    private Button nineButton;
+
+    
+}
