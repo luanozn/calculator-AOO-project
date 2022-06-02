5e2a75b945849eb5400ec4b7f1e93e09ee307e08
Luan de Castro Ribeiro <luan.ribeiro@estudante.ifgoiano.edu.br>
2022-06-02 17:26:06 -0300

    Colocação de ícone

--- src/main/java/com/example/calculator/CalculatorApp.java
+++ src/main/java/com/example/calculator/CalculatorApp.java
@@ -3,16 +3,18 @@ package com.example.calculator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class CalculatorApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("calculator-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApp.class.getResource("calculator-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 230, 330);
-        stage.setTitle("Hello!");
+        stage.setTitle("Calculator");
+        stage.getIcons().add(new Image("https://img.icons8.com/plasticine/100/undefined/apple-calculator.png"));
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
--- src/main/java/com/example/calculator/CalculatorController.java
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -2,10 +2,9 @@ package com.example.calculator;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class HelloController {
+public class CalculatorController {
 
     @FXML
     private TextField operationField;
